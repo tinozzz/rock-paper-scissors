4b6956d10912019dd6f2882d9c4c3f99ba15aaae
Matthew Tolentino <tolentinolmatthew@gmail.com>
2022-03-07 19:43:14 -0500

    Add playRound() function to javascript.js The playRound() passes the user's input and computerPlay result as arguements. The returned result declares the winner of the round.

--- javascript.js
+++ javascript.js
@@ -2,16 +2,41 @@
 function computerPlay(){
 play = Math.floor(Math.random() * 4);
 if (play === 1){
-    return "Rock"
+    return "Rock";
 }
 else if (play === 2){
-    return "Paper"
+    return "Paper";
 }
 else {
-    return "Scissors"
+    return "Scissors";
 }
 }
 
-for (let i = 0; i < 20; i++){
-console.log(computerPlay());
-}
+// for (let i = 0; i < 20; i++){
+// console.log(computerPlay());
+// }
+
+function playRound(playerSelection, computerSelection) {
+    console.log(`Computer chose ${computerSelection}`)
+    playerSelection = playerSelection.charAt(0).toUpperCase()
+    + playerSelection.slice(1).toLowerCase();
+    // console.log(`formated player selection is: ${playerSelection}`);
+    if (playerSelection === computerSelection){
+        return "Draw!";
+    }
+    else if ((playerSelection === "Rock" && computerSelection === "Paper")
+        || (playerSelection === "Paper" && computerSelection === "Scissors")
+        || playerSelection === "Scissors" && computerSelection === "Rock") {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    else {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    }
+    
+
+// playerSelection = prompt("Throw Rock, Paper, or Scissors");
+// console.log(`Player chose ${playerSelection}`);
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
